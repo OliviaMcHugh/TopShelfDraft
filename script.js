f7bfa0e3c5cda20c68d4f95908b057e972eee134
Olivia McHugh <campbellmchugh@students.federation.edu.au>
2017-11-25 12:31:54 +1100

    Init

---
+++ script.js
@@ -0,0 +1,73 @@
+var app = new Vue({
+	el: "#vueApp",
+	data: {
+		users: [
+			{
+				name: "Ash Neill",
+				biography: "My name is Ash Neill, I'm a graphic designer with a massive interest in visual branding and it's connection with psychological influence. I fell in love with design in high school after having a project to create a faux brand as part of one of my classes. Ever since this moment it has become my passion to create well excited Brand identity designs across a large range of clients.",
+				image: "C:\\Users\\mchug\\Documents\\Projects\\Top Shelf\\images\\ashNeill.jpg",
+				projects: [
+				{
+					name: "Altering The Design",
+					description: "The study brings forth a collection of well thought out and executed designs aimed to show viewers that visual branding plays a detrimental role in the successful continuation of a business. Through the development of the overall TOPself  program branding and various other branding projects, this study is an exploration into creating highly thought out designs for a small community group, it is shown how altering the design of a business can shift the cultural landscape of said community, into a place of visually pleasing aesthetics."
+				}
+				]
+			},
+			{
+				name: "Abbey Walsh",
+				biography: "My name is Abbey Walsh and I’m primarily a photographer but I love making short films as well. I like all my work to have a meaning and some depth, I like people to look at my work and go away questioning themselves and their outlooks on life, I get great joy seeing people react to my art. As well as physical art I am also very involved in the performing arts, I act, sing, and dance in my own local area as well as in Melbourne. I love literature particularly the likes of Edgar Allen Poe and The Brothers Grimm, however my favourite books Whinnie The Pooh and Alice In Wonderland.",
+				image: "C:\\Users\\mchug\\Documents\\Projects\\Top Shelf\\images\\abbeyWalsh.jpg",
+				projects: [
+				{
+					name: "Different is Normal",
+					description: "“Different is Normal” I am inviting short film makers of all ages in the Wellington Shire to submit a 2 minute film exploring the concept of individuality through hair. The project is inspired by my own experience growing up in Sale. I don't wear my hair in a ponytail and I have been labelled in my community with words such as “Weird” and “Lesbian” merely because I not afraid to express individuality through hair. I feel Regional towns like Wellington shire pressure individuals to be “cookie cutter” leading to higher rates of depression, anxiety, and suicide. This area turns on anything that is different or new. My project “Normal To Be Different” has an aim of inspiring my region that individuality should be embraced, not shunned. This project aims to provide a platform for creatives to express their expression"
+				}
+				]
+			},
+			{
+				name: "Cassie Leatham-Harrap",
+				biography: "Cassie Leatham-Harrap is from the Taungurung / Wurundjeri people from the Kulin Nation and is also a community member and was born on GunaiKurnai. Cassie explores the bush land and coast around Gippsland and surrounding areas gaining knowledge in edible plants, survival skills, massacre sites and connecting with her heritage. Cassie teaches Bush Tucker in numerous schools in Gippsland and Conducts Workshops teaching skills to Indigenous and Non Indigenous students of all ages. Teaching these workshops to Non Indigenous people give the participants the opportunity to learn and understand the Aboriginal Culture and develop knowledge of both historical and contemporary Aboriginal History. Cassie devotes her time to the local Aboriginal Youth as she feels the responsibility through her workshops and teachings it will nurture and keep the culture alive and to be passed on to generations to come.",
+				image:"C:\\Users\\mchug\\Documents\\Projects\\Top Shelf\\images\\cassieLeathman.jpg",
+				projects: [
+				{
+					name: "Emu Skirts",
+					description: "Tanderrum dancing skirt. Using only traditional techniques and materials from the environment I will be making a new dancing skirt and odorments to wear for this year's Tanderrum at federation square. Materials to be used ; stringybark for my belt, emu feathers, beeswax, ochre, emu skin, possum fur, kangaroo skin, echidna spines, gum nuts, ininti seeds, Bunjil feathers, kookaburra feathers and crow feathers. A emu skin Top will be made along with the dancing skirt. Arm bands also will be made"
+				}
+				]
+			},
+			{
+				name: "Davina Kendell",
+				biography: "My name is Davina Kendall and I have been naturally working with children all my life. I have been a professional dance teacher for over 30 years, with lots of experiences in Melbourne which lead me to teach for Young Talent Time, and performing in the ballroom scene. I moved to the country Stratford and began my own school in 1985 and would say I have taught thousands of children in this time from the age of 15. I had a break for 6months in my late 30’s due to stress fractures in my legs and created a Faerie party business. This is still going. I think I am going to be the oldest faerie ever! After retiring form dance for good, I had to get a real job! I have been in long day care as an educator for 11 years and recently started working from home in family day care for the past two years. After having a three month break from long day care and considering a change in direction as a career, I accidently tripped over the garden spade in the garden and broke my right wrist. Yes more bone cracks....This is where my dream actually came true. I am creative and felt limited in what I could do so I began to draw and write about Elfie.  I drew very small. My wrist actually made me write neater with the cast on. I had also turned 50 and changed my eating habits to no numbers or preservatives or sugar in my life. This started my road to changing my Faerie parties to being all environmentally friendly and not using plastic cheap things from shops, but using our nature to create a better message to the children. As I was searching for an environmental message to complement my lifestyle, I couldn’t find a book about a Faerie at all. I had never found a book that even engaged children in over 18 years let alone a story with a message. What do you do? You write it yourself.  I grew up surrounded by art and creative people as my Grandmother was an artist and so is my Uncle. I am sure she is helping me along with this new direction. I would Love Elfie’s Dream to be as popular as some favourite books like The Bear Hunt. Working with children all my life I understand what engages them and this book does. Elfie’s Dream repeats and I feel has rhythm within the literature.  And she is just so cute! She discovers special things along her journey and the bottom line is she wants her heart to feel love. Who doesn’t? The first book is about her in the garden and will have an audio of me reading the story. Elfie changes into different animals that live in that environment and will discover what is polluting that environment. I would love Elfie to become dolls for sale, maybe an App and I am in the process of having a song written. I will be releasing a new book every three months with the change of the seasons. ",
+				image: "C:\\Users\\mchug\\Documents\\Projects\\Top Shelf\\images\\davinaKendell.jpg",
+				projects: [
+				{
+					name: "Elfies Dream",
+					description: "Elfie’s Dream is about a Faerie that has an awakening to help heal our world and make people aware of how pollution and rubbish can affect the planet. The book was written with a simple message aiming at children who can connect with Elfie and maybe become the next generation in saving the world. This book was written and illustrated from my heart. I am a real life Faerie Earthina and trying to find a Faerie book to read to the children was hard to find that had an environmental message. I searched the world without success and decided to give it a go. Elfie was born. The title Elfie’s Dream is simply her making her dream to save Mother Earth come true. I began by the inspiration of the beautiful garden of Pollyanna’s family house. It is actually 11 Finigan Court. 11 is my lucky number so Elfie’s first book is created here. She then will wonder down to the jetty and connect with the Thomson River and then out to sea and her journey will continue to expand. It is endless and very open ended for a series of books. I am aiming to touch the heart of every child and adult who reads about her and falls in love as I have. I also aim to trigger the minds of children, our younger generation, to be aware of our Planet Earth and am looking at publishing the book as Eco friendly, naturally!! I plan to crowd fund through Top Shelf with Pollyanna to kick start the first of many books. I will be printing some limited editions and creating art work for sale as well as 11 hand drawn Elfie’s."
+				}
+				]
+			},
+			{
+				name: "Jeremy Kasper",
+				biography: "Trained in classical French cuisine in Canada, qualified as a Journeyman Red Seal Chef I had an extensive career that span over twenty years. My menus and dishes have been described as a contemporary dining experience using flavorful seasonal fresh colorful ingredients creating works of Art on a Plate.In 2015 I decided to change career path and pursue my passion for the Arts at RMIT. I have since graduated from the Diploma Of Visual Arts and have continued into Advanced Diploma. Most recently I have been working on my solo exhibition Titled Inertia, which is an Exploration of landscape in motion. My practice involves working across multiple disciplines.",
+				image: "C:\\Users\\mchug\\Documents\\Projects\\Top Shelf\\images\\jermeyKasper.jpg",
+				projects: [
+				{
+				name: "Reclaim Our Lanes",
+				description: "Since 2014 I have been producing street art murals on commission walls and private residence. My murals are illustrative and colorful providing blank walls a new least on life. Through the top shelf program I would like to introduce my Reclaim our Lanes project, which aims to connect with the community and youth through Art. Creating murals around wellington shire inspired by local people their stories and cultural history. This project is working towards brightening up tired walls, removing tags and unwanted graffiti around the community and replace it with works of Art. Being involved in the top shelve program I hope to realize my project “Reclaim our Lane” and I would hope to gain funding through grants and donations to assist in the cost of paints and materials needed to realize this project."
+				}
+				]
+			},
+			{
+				name: "Sarah Finlay",
+				biography: "In 2007 I moved from Ireland to Australia with my man to start a new adventure. After working as a behavioural therapist for children with ASD in Melbourne for 6 years we decided to move to Gippsland and start another new adventure. Moving to Gippsland kick started a journey to live a more simple and natural life which in turn led me to discover the magic of essential oils and started my spiritual adventure. Since starting my own soulful essential oil business through Doterra nearly 2 years ago I developed a passion for living a holistic and guided life and my husband and I founded Simply Living to share our oily love, help others change their lives and to offer Holistic Lifestyle Guidance.",
+				image: "C:\\Users\\mchug\\Documents\\Projects\\Top Shelf\\images\\sarahFinlay.jpg",
+				projects: [
+				{
+				name: "Spiritual Adventures",
+				description: "During my Spiritual Adventure I have discovered a variety of tools that I use myself everyday to support my body, mind and spirit. - Essential Oils, Crystals, positive affirmations and mantras, oracle/tarot cards and meditations. These are all things I would love to teach other people to intertwine into their own lives. While I did study Art and Photography for my final school years I haven't delved into anything creative in years but this seemed like a natural way for me to combine all my passions and knowledge together to share with others."
+				}
+				]
+			}
+		]
+	}
+})
